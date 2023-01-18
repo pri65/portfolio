13171278c214df73e60c96d8dfc7bbf0ab9370fc
pri65 <priyankasoniii65@gmail.com>
2023-01-18 23:33:16 +0530

    New

---
+++ src/data/contactData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: "Rajatsoni.rs66@gmail.com",
+    phone: "+91-97858-64762",
+    address: "Hanumangarh(Raj),India",
+  
+    sheetAPI: "https://sheetdb.io/api/v1/ukao0wm5smohw",
+  };
---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+    {
+      id: 1,
+      institution: "Masai School, Banglore",
+      course: "Full Stack Web Development Course",
+      startYear: "Dec 2021",
+      endYear: "Dec 2022",
+    },
+    {
+      id: 2,
+      institution: "Amity University, Noida",
+      course: "Master of Bussiness Aministration(MBA)",
+      startYear: "Jan 2019",
+      endYear: "Oct 2021",
+    },
+    {
+      id: 3,
+      institution: "RYAN College,Hanumangarh",
+      course: "Bachelor of Bussiness Administration(BBA)",
+      startYear: "June 2015",
+      endYear: "May 2018",
+    },
+  ];
