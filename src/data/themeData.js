f33faf6e4affab63189275df2c2923fe2656d86b
pri65 <priyankasoniii65@gmail.com>
2023-01-18 23:37:10 +0530

    New

---
+++ src/data/themeData.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import {
+    greenThemeLight,
+    greenThemeDark,
+    bwThemeLight,
+    bwThemeDark,
+    blueThemeLight,
+    blueThemeDark,
+    redThemeLight,
+    redThemeDark,
+    orangeThemeLight,
+    orangeThemeDark,
+    purpleThemeLight,
+    purpleThemeDark,
+    pinkThemeLight,
+    pinkThemeDark,
+    yellowThemeLight,
+    yellowThemeDark,
+    purpleMadeAk,
+  } from "../theme/theme";
+  
+  export const themeData = {
+    theme: blueThemeLight,
+  };
+  
