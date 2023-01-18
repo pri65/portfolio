12ce1c8b93e3b9accc9382ff09d094cf46cdb1a6
pri65 <priyankasoniii65@gmail.com>
2023-01-18 23:34:21 +0530

    New

---
+++ src/data/headerData.js
@@ -0,0 +1,10 @@
+import resume from "../assets/pdf/resume.pdf";
+import ProfilePhoto from "../assets/png/ProfilePhoto.png";
+export const headerData = {
+  name: "Rajat Soni",
+  title: "Web Developer",
+  desciption:
+  "I'm a certified web developer who enjoys building interactive interfaces with HTML, JavaScript and React. I'm open to Full-stack Developer remote opportunities with digital-product teams.",
+  image: ProfilePhoto,
+  resumePdf: resume,
+};
