d9454eca311e87d93752cda7062b313e61c2fff3
pri65 <priyankasoniii65@gmail.com>
2023-01-19 16:19:10 +0530

    New

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -212,10 +212,10 @@ function Navbar() {
             </Fade>
 
             <Fade left>
-              <NavLink to="/#blog" smooth={true} spy="true" duration={2000}>
+              <NavLink to="/#experience" smooth={true} spy="true" duration={2000}>
                 <div className={classes.drawerItem}>
                   <FaFolderOpen className={classes.drawerIcon} />
-                  <span className={classes.drawerLinks}>Blog</span>
+                  <span className={classes.drawerLinks}>Experience</span>
                 </div>
               </NavLink>
             </Fade>
